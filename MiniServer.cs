26625d6aec2dd0b33b8b9b120cb05c58222c1101
Liliyaa11 <120708561+Liliyaa11@users.noreply.github.com>
2023-10-23 13:25:22 +0300

    Add files via upload

---
+++ MiniServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Surver.Exceptions
+{
+    internal class stringExaptions
+    {
+        public static Capitalize(CallConvThiscall string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+            return char.ToUpper(text[0]) + text.Substring(1).ToLower();
+
+        }
+    }
+}
